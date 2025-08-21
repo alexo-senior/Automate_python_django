9dce5f443fd79dddf6e1ded174989e77d5a5d810
alexis <alexobarrafer@gmail.com>
2025-08-21 18:45:18 -0500

    se añadio el apellido del usuario en el saludo

--- dataentry/management/commands/greeting.py
+++ dataentry/management/commands/greeting.py
@@ -6,18 +6,22 @@ from django.core.management.base import BaseCommand
 #proposed output Hi{name}, Good Morning
 
 class Command(BaseCommand):
-    help = "Greets the User"
+    help = "Greets to the Users Everyday"
     
     def add_arguments(self, parser):
-        parser.add_argument('name', type=str, help='Specifies user name')
+        parser.add_argument('name',  type=str, help='Specifies user name')
+        parser.add_argument('lastname',  type=str, help='Specifies user lastname')
         
         
     #funcion principal
     def handle(self, *args, **kwargs):
         #se usa kwargs porque el nombre es una palabra clave y acepta cualquiera
+        #se puede añadir el apellido con igual logica
         name = kwargs['name']
-        greeting = f'Hi {name}, Good Morning!'
+        lastname = kwargs['lastname']
+        greeting = f'Hi {name} {lastname}, Good Morning!'
         #self.stdout.write(greeting)
     #si quiero mostrar como un mensaje de exito en la consola
     #sale en color verde y en formato negrita
         self.stdout.write(self.style.SUCCESS(greeting))
+        
